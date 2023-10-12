3181ee99af7c97af8d40a76c31e79939f0c99a98
Thommy (Tommy, Tom) <thjomaz@icloud.com>
2023-10-12 22:31:35 +0200

    WIP: editor sidebar

--- app/projects/[id]/editor/page.tsx
+++ app/projects/[id]/editor/page.tsx
@@ -1,28 +1,36 @@
-'use client'
+'use client';
 
-import React from "react";
+import React, { useState } from 'react';
 
 export default function Editor() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
   return (
-    <>
-      <div className="py-10">
-        <header>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Editor</h1>
-          </div>
-        </header>
+    <div className="flex h-screen bg-gray-100">
+      <main className={`relative flex flex-1 flex-col overflow-hidden ${isSidebarVisible ? 'md:w-3/4' : 'w-full'}`}>
+        {/* Main Content */}
+        Content here...
+        {/* Sidebar Toggle Button for smaller screens */}
+        <button className="absolute right-5 top-5 md:hidden" onClick={() => setIsSidebarVisible(!isSidebarVisible)}>
+          Toggle Sidebar
+        </button>
+      </main>
 
-        <main className="flex flex-col gap-10">
-
-          {/*<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">*/}
-          {/*  <EmptyState*/}
-          {/*    title="No projects"*/}
-          {/*    description="Create a new project in 5 minutes"*/}
-          {/*    buttonText="New project"*/}
-          {/*  />*/}
-          {/*</div>*/}
-        </main>
-      </div>
-    </>
-  )
+      <Sidebar isSidebarVisible={false} />
+    </div>
+  );
 }
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+const Sidebar: React.FC<{ isSidebarVisible: boolean }> = ({ isSidebarVisible }) => {
+  console.log(isSidebarVisible);
+  return (
+    <aside className="absolute right-0 hidden h-full w-64 overflow-auto bg-white md:block md:w-[300px]">
+      {/* Sidebar Content */}
+      Sidebar content...
+    </aside>
+  );
+};
